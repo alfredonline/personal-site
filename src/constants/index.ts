bbb3c93b60eae74430d626b7ad8c0e0d9f408373
Alfie <87819682+alfredonline@users.noreply.github.com>
2025-12-15 17:40:16 +0000

    simplify the whole thing

--- src/constants/index.ts
+++
@@ -1,6 +0,0 @@
-// This file is intentionally left empty
-
-// Create a central export file for all constants
-export * from './navlinks';
-export * from './socials';
-export * from './featuredProjects'; 
